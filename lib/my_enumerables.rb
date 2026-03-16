85f4a3d16a3d134be86ad5fefd49c77adc633dbd
bjornrohrer <113996059+bjornrohrer@users.noreply.github.com>
2026-03-16 21:27:00 +0100

    Added my_each and my_all?

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,15 @@
 module Enumerable
-  # Your code goes here
+  def my_all?
+    my_each { |element| return false unless yield(element) }
+    true
+  end
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
-  # Define my_each here
+  def my_each
+    for element in self
+      yield(element)
+    end
+    self
+  end
 end
